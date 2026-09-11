2a5038d6d7615b522be163a46c1dc8a6610c8af5
Allan Jardine <allan@datatables.net>
2026-09-11 12:37:10 +0000

    Release 3.0.2

--- datatables_server/editor.py
+++ datatables_server/editor.py
@@ -271,7 +271,7 @@ class Editor(NestedData):
     """
 
     Action = Action
-    version = "3.0.1"
+    version = "3.0.2"
 
     # ------------------------------------------------------------------
     # Constructor
